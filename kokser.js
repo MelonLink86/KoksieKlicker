54b6dea345eefc4e815fdbfb8edd31e200352d6f
MelonLink86 <melonfire44@gmail.com>
2022-05-22 20:56:18 +0200

    187

--- kokser.js
+++ kokser.js
@@ -22,26 +22,31 @@ class kokser {
     }
 
     update(frameCount,x,y){
-       fill(145, 23, 23);
-       rect(x,y,400,100);
 
+     //kredidkarte
+      colorMode(HSB, 100);
+       fill(y/8, 100, 80);
+       colorMode(RGB, 255);
+       rect(x,y,400,100);
        fill(0);
        textSize(30);
        textAlign(LEFT);
        text(this.name, x+10, y+40);
        textSize(20);
-       text('kk: ' +this.getPreis(), x+10, y+75);
+       text('kk: ' +(nuttation(this.getPreis())), x+10, y+75);
        textSize(50);
        textAlign(RIGHT,CENTER);
-       text(this.menge, x+390, y+50)
+       text(nuttation(this.menge), x+390, y+50);
+      
 
-
-       
 
        if (frameCount%this.krate == 0){
             return this.kzuwachs*this.menge;
-        }else return 0;
-        
+        }   else return 0;
+
+
+
+     
         
         
     }
